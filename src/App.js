498195041e6cdbb4a7ce1d7368bbbe4360eb7da5
ellesse80 <ellesse80@gmail.com>
2021-06-21 21:25:00 +0900

    https://nomadcoders.co/react-fundamentals/lectures/1554 3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
-import PropTypes from "prop-types"
 
+/**
+ * React.Component 가 갖고 있는 function
+ * - Life Cycle function ( render() 전 호출 )
+ *    -- Mounting : "태어나는 것"과 같아
+ *          * constructor()
+ *          * static getDerivedStateFromProps() -패스
+ *          * render()
+ *          * componentDidMount()
+ *    -- Updating : 일반적인 업데이트를 의미
+ *           * static getDerivedStateFromProps()
+ *           * shouldComponentUpdate() -패스 ( 업데이트 할지 말지 결정하는 아이 )
+ *           * render()
+ *           * getSnapshotBeforeUpdate() -패스 ( 노마드 인생에 사용한 적 없다 함 ㅋ )
+ *           * componentDidUpdate()
+ *    -- UnMounting : component가 죽는걸 의미 ( 페이지가 바뀔때 )
+ *            * componentWillUnmount()
+ * - render()
+*/
 class App extends React.Component{
+  constructor(){
+    super();
+    console.log("Hello");
+  }
   // state 을 사용 하려면 React.Component 안에서 사용해야 함
   state = {
     count: 0
@@ -23,7 +44,20 @@ class App extends React.Component{
     }));
   };
 
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+  
   render(){
+    console.log("React");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
