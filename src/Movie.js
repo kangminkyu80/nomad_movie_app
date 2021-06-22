17953d87c26614370a634764084c794c35364caf
ellesse80 <ellesse80@gmail.com>
2021-06-22 21:55:02 +0900

    https://nomadcoders.co/react-fundamentals/lectures/1561 4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie ({ id, title, year, summary, poster }) {
+function Movie ({ id, title, year, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img key={id} src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {
+                        genres.map((genre, index) => 
+                            <li key={index} className="genres__genre">
+                                {genre}
+                            </li>
+                        )
+                    }
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
@@ -20,7 +29,8 @@ Movie.propTypes = {
     year: PropTypes.number,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
